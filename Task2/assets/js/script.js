a2861d2d0200f51950184eaab2568e1eba509937
Khalitov62 <tu7m5rk73code.edu.az>
2022-05-04 14:47:13 +0400

    added

---
+++ Task2/assets/js/script.js
@@ -0,0 +1,36 @@
+// Task 7. Verilmis arrayin icerisinde deyeri 45 olan elementin indexini tapan function
+
+// let arr = [1, 2, 3, 4, 5, 6, 45];
+// let position=arr.indexOf(45);
+// console.log(position);
+
+//Task 6. Verilmis M ededini verilmis N quvvete yukselden function
+
+// function Power(M,N){
+//     return M**N;
+// }
+// console.log(Power(5 ,2));
+
+//Task 1.  Her hansı bir stringin içərisindəki yazıları söz sırası və hərf sırası daxil olmaqla tərs yazdıran bir function yazın
+
+// function Reverse (word){
+//   return word.split("").reverse().join("")
+// }
+// console.log(Reverse("mehemmed deniz"));
+
+//Task 5.Verilmis n ededinin son reqemini onun evveline getirerek yeni bir eded duzelden function (misalcun daxil edilen eded 475 olarsa, geriye 547 qaytarsin)
+// function Reverse(n){
+//     var remainder,
+//     result = 0
+
+//     while( n ){
+//         remainder = n % 10
+//         result = (result * 10) + remainder
+//         n = n/10|0
+//     }
+
+//     return result
+// }
+
+// console.log(Reverse(475 ));
+
